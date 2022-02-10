e65545644c102cfbc9cff2a7b858cdcb88e30c65
ddewe <ddewee92@gmail.com>
2022-02-10 14:31:26 +0100

    added status field with color depending on status

--- src/components/showTodoList.jsx
+++ src/components/showTodoList.jsx
@@ -1,37 +1,47 @@
-import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import axios from "axios";
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 function TodoCard({ data }) {
-  const { _id, title } = data;
+  const { _id, title, status } = data
+
   return (
     <li key={_id}>
       <div className="title-description">
         <h3>{title}</h3>
+        <h5
+          style={
+            status === 'Unfinished' ? { color: 'red' } : { color: 'green' }
+          }
+        >
+          {status}
+        </h5>
       </div>
 
       <div className="button-container">
-        <button className="button">edit</button>
+        <button name={_id} className="button">
+          edit
+        </button>
         <button className="button">delete</button>
       </div>
     </li>
-  );
+  )
 }
 
 export function ShowTodoList() {
-  const [todo, setTodo] = useState([]);
+  const [todo, setTodo] = useState([])
 
   useEffect(() => {
     axios
-      .get("http://localhost:8000/api/task")
-      .then((res) => {
-        console.log(res.data);
-        setTodo(res.data);
+      .get('http://localhost:8000/api/task')
+      .then(res => {
+        console.log(res.data)
+        setTodo(res.data)
       })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
 
   return (
     <section className="container">
@@ -41,11 +51,11 @@ export function ShowTodoList() {
       <section className="contents">
         <h1>TASK</h1>
         <ul className="list-container">
-          {todo.map((data) => (
+          {todo.map(data => (
             <TodoCard data={data} key={data._id} />
           ))}
         </ul>
       </section>
     </section>
-  );
+  )
 }
