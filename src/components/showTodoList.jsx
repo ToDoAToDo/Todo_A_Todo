e9a810389bcd061017d0cdb15bfc588de02c9297
ddewe <ddewee92@gmail.com>
2022-02-15 14:29:27 +0100

    added functionality to complete a task

--- src/components/showTodoList.jsx
+++ src/components/showTodoList.jsx
@@ -3,18 +3,30 @@ import { Link } from 'react-router-dom'
 import axios from 'axios'
 
 function TodoCard({ data }) {
-  const { _id, title, status } = data
+  const { _id, title, completed } = data
+
+  const [isCompleted, setIsCompleted] = useState(completed)
+
+  function handleClick() {
+    let newStatus = data //The data object to change status of
+    newStatus.completed = !newStatus.completed //Sets the value of the status to the opposite of previous value
+    axios
+      .put(`http://localhost:8000/api/task/${_id}`, newStatus)
+      .then(res => {
+        setIsCompleted(!completed)
+      })
+      .catch(err => {
+        console.log('Failed to update todo')
+        console.log(err.message)
+      })
+  }
 
   return (
     <li key={_id}>
       <div className="title-description">
         <h3>{title}</h3>
-        <h5
-          style={
-            status === 'Unfinished' ? { color: 'red' } : { color: 'green' }
-          }
-        >
-          {status}
+        <h5 style={completed === false ? { color: 'red' } : { color: 'green' }}>
+          {completed ? 'Finished' : 'Unfinished'}
         </h5>
       </div>
 
@@ -23,6 +35,7 @@ function TodoCard({ data }) {
           edit
         </button>
         <button className="button">delete</button>
+        <button onClick={() => handleClick()}>finish</button>
       </div>
     </li>
   )
@@ -34,11 +47,10 @@ export function ShowTodoList() {
   useEffect(() => {
     axios
       .get('http://localhost:8000/api/task')
-      .then((res) => {
-        console.log(res.data)
+      .then(res => {
         setTodo(res.data)
       })
-      .catch((err) => {
+      .catch(err => {
         console.log(err)
       })
   }, [])
@@ -51,7 +63,7 @@ export function ShowTodoList() {
       <section className="contents">
         <h1>TASK</h1>
         <ul className="list-container">
-          {todo.map((data) => (
+          {todo.map(data => (
             <TodoCard data={data} key={data._id} />
           ))}
         </ul>
