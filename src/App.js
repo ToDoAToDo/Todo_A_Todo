860e2830ce17eb15b141ba5a8250684122cb8b46
jakob.lundberg1 <jakoblundbergsu15@gmail.com>
2022-02-16 11:10:23 +0100

    Added Design for Task Cards and a Navbar

--- src/App.js
+++ src/App.js
@@ -4,16 +4,23 @@ import { CreateTodo } from './components/createTodo'
 import { ShowTodoList } from './components/showTodoList'
 import { UserList } from './components/user/user'
 import { CreateUserForm } from './components/user/createUserForm'
+import carddesign from './components/carddesign/cardDesign'
+import navbar from './components/navbar'
+const cardDesign = new carddesign()
+const navBar = new navbar()
 
 function App() {
   return (
     <div className="app-contents">
+      {navBar.getNavBar()}
+
       <BrowserRouter>
         <Route exact path="/" component={ShowTodoList} />
         <Route path="/create-todo" component={CreateTodo} />
         <Route exact path="/user" component={UserList} />
         <Route exact path="/user/new" component={CreateUserForm} />
       </BrowserRouter>
+      {cardDesign.getCardDesign('Glenn', 'Dishes')}
     </div>
   )
 }
